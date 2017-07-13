8c1359c2f0edea82d9c18fdf2dfeb6d929d7659e
Joe Ferner <joe@fernsroth.com>
2017-07-13 18:36:41 -0400

    Fixed: react checkbox value to checked

--- web/war/src/main/webapp/js/workspaces/userAccount/bundled/settings/BooleanSetting.jsx
+++ web/war/src/main/webapp/js/workspaces/userAccount/bundled/settings/BooleanSetting.jsx
@@ -67,7 +67,7 @@ define([
         render() {
             return (<label>
                 <div className="display-name">{i18n(this.props.setting.displayName)}</div>
-                <input type="checkbox" disabled={this.state.disabled} value={this.state.value}
+                <input type="checkbox" disabled={this.state.disabled} checked={this.state.value}
                        onChange={this.handleCheckboxChange}/>
             </label>);
         }
